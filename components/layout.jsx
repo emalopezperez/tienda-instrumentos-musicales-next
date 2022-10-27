485ce2b3dd5c9646ba3c2bf0048cebb7008a221d
emanuel lopez <emanuel-lopez13@hotmail.com>
2022-10-26 21:34:26 -0300

    comenzando con el proyecto

---
+++ components/layout.jsx
@@ -0,0 +1,11 @@
+
+const Layout = ({children}) => {
+  return (
+    <>
+      {children}
+      <p>desde children</p>
+    </>
+  )
+}
+
+export default Layout
---
+++ pages/nosotros.jsx
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+import Link from "next/link";
+
+const nosotros = () => {
+  return (
+    <Layout>
+      <h2>Nosotros</h2>
+      <Link href="/">Home </Link>
+    </Layout>
+  );
+};
+
+export default nosotros;
